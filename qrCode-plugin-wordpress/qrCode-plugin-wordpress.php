21c78ec14c1c9244af04e2824b07165635d8a8c8
h4shell <h4shell@gmail.com>
2024-02-23 12:01:46 +0100

    Fix missing character

--- qrCode-plugin-wordpress/qrCode-plugin-wordpress.php
+++ qrCode-plugin-wordpress/qrCode-plugin-wordpress.php
@@ -24,25 +24,24 @@ if (!defined('ABSPATH')) {
 
 function myplugin_banner()
 {
-  echo "\
-    <div class=\"myplugin-banner-container myplugin-hidden\">
-      <p class=\"myplugin-banner-text\">QRCODE</p>
-      <div id=\"qrcode\"></div>
-      <div class=\"myplugin-banner-button\">
-        <svg
-          xmlns=\"http://www.w3.org/2000/svg\"
-          width=\"36\"
-          height=\"36\"
-          fill=\"currentColor\"
-          class=\"bi bi-caret-right-fill\"
-          viewBox=\"0 0 16 16\"
-        >
-          <path
-            d=\"m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z\"
-          />
-        </svg>
-      </div>
-    </div>
+  echo "<div class=\"myplugin-banner-container myplugin-hidden\">
+          <p class=\"myplugin-banner-text\">QRCODE</p>
+          <div id=\"qrcode\"></div>
+          <div class=\"myplugin-banner-button\">
+            <svg
+              xmlns=\"http://www.w3.org/2000/svg\"
+              width=\"36\"
+              height=\"36\"
+              fill=\"currentColor\"
+              class=\"bi bi-caret-right-fill\"
+              viewBox=\"0 0 16 16\"
+            >
+              <path
+                d=\"m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z\"
+              />
+            </svg>
+          </div>
+        </div>
     ";
 }
 
